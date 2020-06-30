23b77eccaea16763c341ec937feb2977be0d914a
Shreyas Ikhar <40795917+shreyasikhar@users.noreply.github.com>
2020-06-30 21:11:09 +0530

    Update sendmsg.php

--- sendmsg.php
+++ sendmsg.php
@@ -58,7 +58,7 @@ if (strlen($fname) > 50) {
     $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
     $mail->Username = 'enlectic@gmail.com';                 // SMTP username
-    $mail->Password = 'Enlectic@123';                           // SMTP password
+    $mail->Password = '';                           // SMTP password
     $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
     $mail->Port = 587;                                    // TCP port to connect to
 
